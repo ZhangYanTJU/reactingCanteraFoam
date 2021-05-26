8210be7bf1b9ade907301e665b48ad11756a7c78
ZhangYanTJU <zytju@tju.edu.cn>
2021-05-26 19:03:49 +0200

    use rho instead gas->density in D; add one thermoCorrectByCantera during initialization

--- reactingCanteraFoam.C
+++ reactingCanteraFoam.C
@@ -71,6 +71,7 @@ int main(int argc, char *argv[])
     #include "initContinuityErrs.H"
     #include "createFields.H"
     #include "createFieldRefs.H"
+    #include "thermoCorrectByCantera.H"
 
     turbulence->validate();
 
